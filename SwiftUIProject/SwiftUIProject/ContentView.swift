6833871f76c00c90132159384848ca63a57675f6
dinnismary <w1605356103@163.com>
2025-03-18 21:51:08 +0800

    create project

---
+++ SwiftUIProject/SwiftUIProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIProject
+//
+//  Created by Mac on 2025/3/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIProject/SwiftUIProject/SwiftUIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIProjectApp.swift
+//  SwiftUIProject
+//
+//  Created by Mac on 2025/3/18.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
